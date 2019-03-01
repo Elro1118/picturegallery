29fce6d00baf654f938953a243eec48a88e6f3b5
Elro1118 <silvana.rojas8119@gmail.com>
2019-03-01 07:44:44 -0500

    improvingPhase:StylingPage

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import data from '../data/data.json'
 import { Link } from 'react-router-dom'
+import { spawn } from 'child_process'
 
 class CategoryList extends Component {
   render() {
@@ -9,23 +10,27 @@ class CategoryList extends Component {
     return (
       <>
         <div className="header-section">
-          <h1 className="title">Things I Like</h1>
+          <h1 className="title">
+            <a href="/">Things I Like</a>
+          </h1>
           <h2 className="subtitle">A Photo Gallery by Jason L Perry</h2>
         </div>
-        <div>
-          <nav>
+        <div className="main-section">
+          <nav className="menu-section">
             <ul>
-              <li>Home</li>
+              <li>
+                <a href="http://things-i-like.surge.sh/">🏠Home</a>
+              </li>
             </ul>
           </nav>
-          <div>
+          <div className="pictures-section">
             <div>
-              <ul>
+              <ul className="pictures">
                 {Object.keys(data).map(category => {
                   return (
                     <li>
                       <Link to={`/${category}`}>
-                        <h3>{category}</h3>
+                        <h3>{data[category].title}</h3>
                       </Link>
                       <p>{data[category].description}</p>
                       <img src={data[category].photos[0].imageURL} />
